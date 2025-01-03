2c8e70755e4ce6e18f922a5f4538e55fc4aa18f8
paoloprogram12 <sethpsalazar@gmail.com>
2025-01-02 23:43:04 -0800

    hg

--- main.py
+++ main.py
@@ -5,3 +5,22 @@ pygame.init()
 
 screenWidth = 864
 screenHeight = 936
+
+# game window
+screen = pygame.display.set_mode((screenWidth, screenHeight))
+pygame.display.set_caption("Flappy Bird")
+
+# loads images
+background = pygame.image.load("bg.png")
+
+run = True
+while run:
+
+    # displays background img
+    screen.blit(background, (0,0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
