708785a36629fe9f081463b7d5ecbf164b005484
paoloprogram12 <sethpsalazar@gmail.com>
2025-03-08 11:03:48 -0800

    added some comments

--- main.py
+++ main.py
@@ -4,23 +4,23 @@ from pygame.locals import *
 pygame.init()
 
 # sets time
-clock = pygame.time.Clock()
-fps = 60
+clock = pygame.time.Clock() # creates a timer
+fps = 60 # framerate for the game
 
 screenWidth = 864
 screenHeight = 936
 
 # game window
-screen = pygame.display.set_mode((screenWidth, screenHeight))
-pygame.display.set_caption("Flappy Bird")
+screen = pygame.display.set_mode((screenWidth, screenHeight)) # the dimensions for the window of the game
+pygame.display.set_caption("Flappy Bird") # title for the game
 
 # game variables
-groundScroll = 0
-scrollSpeed = 4 # moves by 4 px
+groundScroll = 0 # used for the ground image to change its coordinates
+scrollSpeed = 4 # moves by 4 px (used for how fast should the ground be moving)
 
 # loads images
-background = pygame.image.load("bg.png")
-ground = pygame.image.load("ground.png")
+background = pygame.image.load("bg.png") # variable for the background img
+ground = pygame.image.load("ground.png") # variable for the ground img
 
 # The bird itself
 class Bird(pygame.sprite.Sprite):
@@ -57,22 +57,26 @@ bird_group.add(flappy)
 
 
 run = True
-while run:
+while run: # runs the flappy bird game
 
+    # sets the game loop locked to certain fps (60)
     clock.tick(fps)
 
     # draw background
-    screen.blit(background, (0,0))
+    screen.blit(background, (0,0)) # sets the background to background
 
     bird_group.draw(screen)
     bird_group.update()
     
     # draw and scrolls the ground
-    screen.blit(ground, (groundScroll, 768))
-    groundScroll -= scrollSpeed
+    screen.blit(ground, (groundScroll, 768)) # adds ground img to the game
+    groundScroll -= scrollSpeed # allows the ground img to look like it is moving
+    # resets the x coordinate to once it becomes > than 35
     if abs(groundScroll) > 35:
         groundScroll = 0
 
+
+    # quits the game (when the x button on the right is clicked)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
